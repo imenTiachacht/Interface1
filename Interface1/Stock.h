dcfb8f331912adf1e6e0c1a16d071957926543b6
THT <THT@DESKTOP-UKIUJ7S>
2020-11-30 14:38:42 +0100

    essai

--- Interface1/Stock.h
+++ Interface1/Stock.h
@@ -34,6 +34,17 @@ namespace Interface1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	protected:
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::TextBox^ textBox6;
+	private: System::Windows::Forms::TextBox^ textBox7;
+	private: System::Windows::Forms::TextBox^ textBox8;
+	private: System::Windows::Forms::TextBox^ textBox9;
 
 	private:
 		/// <summary>
@@ -48,11 +59,114 @@ namespace Interface1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Stock";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox8 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox9 = (gcnew System::Windows::Forms::TextBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(342, 131);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersWidth = 62;
+			this->dataGridView1->RowTemplate->Height = 28;
+			this->dataGridView1->Size = System::Drawing::Size(654, 415);
+			this->dataGridView1->TabIndex = 0;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(23, 23);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(170, 26);
+			this->textBox1->TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(846, 75);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(170, 26);
+			this->textBox2->TabIndex = 2;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(576, 75);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(170, 26);
+			this->textBox3->TabIndex = 3;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(309, 75);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(170, 26);
+			this->textBox4->TabIndex = 4;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(846, 23);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(170, 26);
+			this->textBox5->TabIndex = 5;
+			// 
+			// textBox6
+			// 
+			this->textBox6->Location = System::Drawing::Point(23, 179);
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(170, 26);
+			this->textBox6->TabIndex = 6;
+			// 
+			// textBox7
+			// 
+			this->textBox7->Location = System::Drawing::Point(309, 23);
+			this->textBox7->Name = L"textBox7";
+			this->textBox7->Size = System::Drawing::Size(170, 26);
+			this->textBox7->TabIndex = 7;
+			// 
+			// textBox8
+			// 
+			this->textBox8->Location = System::Drawing::Point(576, 23);
+			this->textBox8->Name = L"textBox8";
+			this->textBox8->Size = System::Drawing::Size(170, 26);
+			this->textBox8->TabIndex = 8;
+			// 
+			// textBox9
+			// 
+			this->textBox9->Location = System::Drawing::Point(23, 100);
+			this->textBox9->Multiline = true;
+			this->textBox9->Name = L"textBox9";
+			this->textBox9->Size = System::Drawing::Size(170, 26);
+			this->textBox9->TabIndex = 9;
+			// 
+			// Stock
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1063, 575);
+			this->Controls->Add(this->textBox9);
+			this->Controls->Add(this->textBox8);
+			this->Controls->Add(this->textBox7);
+			this->Controls->Add(this->textBox6);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->dataGridView1);
+			this->Name = L"Stock";
+			this->Text = L"Stock";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
 	};
