eef2f7ccca4dcfba4c8c54184e62bc1aa6cee015
RayCookie <45881039+RayCookie@users.noreply.github.com>
2020-11-30 17:28:55 +0100

    hey there delilah !!

--- Interface1/Cl_commande.cpp
+++ Interface1/Cl_commande.cpp
@@ -1,39 +1,101 @@
 #include "Cl_commande.h"
 
-void Cl_commande::afficher()
-{ //sql connetion 
-	String^ constring = "Data Source=LAPTOP-PU1057KM\SQLEXPRESS;Initial Catalog=code lyoko;Integrated Security=True";
-	SqlConnection^ conDataBase = gcnew SqlConnection(constring);
-	//lecture du element de bdd (formulaire) tableau ?! 
+namespace NS_Composants
+{
+	Cl_commande::Cl_commande(void)
+	{
+		this->id_commande = -1;
+		this->adresse = "RIEN";
+		this->DateReglement = "RIEN";
+		this->DatePay= "RIEN";
+		this->DateEmi = "RIEN";
+		this->DateLiv = "RIEN";
+		this->id_client = -1;
+		this->id_article = -1;
+	}
+	String^ Cl_commande::SELECT(void)
+	{
+		/*return "SELECT id_adresse, adresse, ville, cp, id_personne " +
+			"FROM TB_ADRESSE " +
+			"WHERE(id_personne = " + this->id_personne + ");"; */
+	}
+	String^ Cl_commande::INSERT(void)
+	{/*
+		return "INSERT INTO TB_ADRESSE(adresse, ville, cp, id_personne) " +
+			"VALUES('" + this->adresse + "', '" + this->ville + "', '" + this->cp + "', " + this->id_personne + ");";*/
+	}
+	String^ Cl_commande::UPDATE(void)
+	{/*
+		return "UPDATE TB_ADRESSE " +
+			"SET adresse ='" + this->adresse + "', ville ='" + this->ville + "', cp ='" + this->cp + "' " +
+			"WHERE(id_adresse = " + this->id_adresse + "); "; */
+	}
+	String^ Cl_commande::DELETE(void)
+	{/*
+		return "DELETE FROM TB_ADRESSE " +
+			"WHERE(id_adresse = " + this->id_adresse + ");"; */ 
+	}
+	void Cl_commande::setId_commande(int Id_commande)
+	{
+		//if (IdAdresse > 0)this->id_adresse = IdAdresse;
+	}
+	void Cl_commande::setAdresse(String^ Adresse)
+	{
+		//if (Adresse != "")this->adresse = Adresse;
+	}
+	void Cl_commande::setDateReglement(String^ DateReglement)
+	{
+		//if (Cp != "")this->cp = Cp;
+	}
+	void Cl_commande::setDatePay(String^ DatePay)
+	{
+		//if (Ville != "")this->ville = Ville;
+	}
+	void Cl_commande::setDateEmi(String^ DateEmi)
+	{
+		//if (IdPersonne > 0)this->id_personne = IdPersonne;
+	}
+	void Cl_commande::setId_client(int Id_client)
+	{
+		//if (IdAdresse > 0)this->id_adresse = IdAdresse;
+	}
+	void Cl_commande::setId_article(int Id_article)
+	{
+		//if (IdAdresse > 0)this->id_adresse = IdAdresse;
+	}
 
-	//requete sql 
+	int Cl_commande::getId_commande(void)
+	{
+		//return this->id_adresse;
+	}
+	String^ Cl_commande::getAdresse(void)
+	{
+		//return this->adresse;
+	}
+	String^ Cl_commande::getDateReglement(void)
+	{
+		//return this->cp;
+	}
+	String^ Cl_commande::getDatePay(void)
+	{
+		//return this->ville;
+	}
+	String^ Cl_commande::getDateEmi(void)
+	{
+		//return this->ville;
+	}
+	String^ Cl_commande::getDateLiv(void)
+	{
+		//return this->ville;
+	}
+	
+	int Cl_commande::getId_client(void)
+	{
+		//return this->id_adresse;
+	}
 
-	//ajout dans Vector 
-}
-
-void Cl_commande::modifier()
-{ //sql connetion 
-	String^ constring = "Data Source=LAPTOP-PU1057KM\SQLEXPRESS;Initial Catalog=code lyoko;Integrated Security=True";
-	SqlConnection^ conDataBase = gcnew SqlConnection(constring);
-	//lecture du element du formulaire ?! 
-	//requete sql (ecraser les données de l'element ) ana chbab 
-	//ajout dans Vector
-}
-
-void Cl_commande::ajouter()
-{ //sql connetion 
-	String^ constring = "Data Source=LAPTOP-PU1057KM\SQLEXPRESS;Initial Catalog=code lyoko;Integrated Security=True";
-	SqlConnection^ conDataBase = gcnew SqlConnection(constring);
-	//lecture de l'element du formulaire ?! 
-	//requete sql (ajouter l'element dans la base de données ) 
-	//ajout dans Vector
-}
-
-void Cl_commande::supprimer()
-{ //sql connetion 
-	String^ constring = "Data Source=LAPTOP-PU1057KM\SQLEXPRESS;Initial Catalog=code lyoko;Integrated Security=True";
-	SqlConnection^ conDataBase = gcnew SqlConnection(constring);
-	//lecture de l'element du formulaire ?! 
-	//requete sql (suppression personne ) 
-	//ajout dans Vector
+	int Cl_commande::getId_article(void)
+	{
+		//return this->id_adresse;
+	}
 }
