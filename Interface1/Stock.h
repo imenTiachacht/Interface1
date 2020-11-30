90ef62c706f4ffe3b173bd08c0e1ca4df9039f88
AhmedNazim <nazimgaoua2001@gmail.com>
2020-11-30 16:13:47 +0100

    nhabek sabrina

--- Interface1/Stock.h
+++ Interface1/Stock.h
@@ -75,82 +75,92 @@ namespace Interface1 {
 			// dataGridView1
 			// 
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(342, 131);
+			this->dataGridView1->Location = System::Drawing::Point(134, 73);
+			this->dataGridView1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->dataGridView1->Name = L"dataGridView1";
 			this->dataGridView1->RowHeadersWidth = 62;
 			this->dataGridView1->RowTemplate->Height = 28;
-			this->dataGridView1->Size = System::Drawing::Size(654, 415);
+			this->dataGridView1->Size = System::Drawing::Size(436, 270);
 			this->dataGridView1->TabIndex = 0;
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(23, 23);
+			this->textBox1->Location = System::Drawing::Point(15, 15);
+			this->textBox1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(170, 26);
+			this->textBox1->Size = System::Drawing::Size(115, 20);
 			this->textBox1->TabIndex = 1;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(846, 75);
+			this->textBox2->Location = System::Drawing::Point(564, 49);
+			this->textBox2->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(170, 26);
+			this->textBox2->Size = System::Drawing::Size(115, 20);
 			this->textBox2->TabIndex = 2;
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(576, 75);
+			this->textBox3->Location = System::Drawing::Point(384, 49);
+			this->textBox3->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(170, 26);
+			this->textBox3->Size = System::Drawing::Size(115, 20);
 			this->textBox3->TabIndex = 3;
 			// 
 			// textBox4
 			// 
-			this->textBox4->Location = System::Drawing::Point(309, 75);
+			this->textBox4->Location = System::Drawing::Point(206, 49);
+			this->textBox4->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(170, 26);
+			this->textBox4->Size = System::Drawing::Size(115, 20);
 			this->textBox4->TabIndex = 4;
 			// 
 			// textBox5
 			// 
-			this->textBox5->Location = System::Drawing::Point(846, 23);
+			this->textBox5->Location = System::Drawing::Point(564, 15);
+			this->textBox5->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->textBox5->Name = L"textBox5";
-			this->textBox5->Size = System::Drawing::Size(170, 26);
+			this->textBox5->Size = System::Drawing::Size(115, 20);
 			this->textBox5->TabIndex = 5;
 			// 
 			// textBox6
 			// 
-			this->textBox6->Location = System::Drawing::Point(23, 179);
+			this->textBox6->Location = System::Drawing::Point(15, 116);
+			this->textBox6->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->textBox6->Name = L"textBox6";
-			this->textBox6->Size = System::Drawing::Size(170, 26);
+			this->textBox6->Size = System::Drawing::Size(115, 20);
 			this->textBox6->TabIndex = 6;
 			// 
 			// textBox7
 			// 
-			this->textBox7->Location = System::Drawing::Point(309, 23);
+			this->textBox7->Location = System::Drawing::Point(206, 15);
+			this->textBox7->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->textBox7->Name = L"textBox7";
-			this->textBox7->Size = System::Drawing::Size(170, 26);
+			this->textBox7->Size = System::Drawing::Size(115, 20);
 			this->textBox7->TabIndex = 7;
 			// 
 			// textBox8
 			// 
-			this->textBox8->Location = System::Drawing::Point(576, 23);
+			this->textBox8->Location = System::Drawing::Point(384, 15);
+			this->textBox8->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->textBox8->Name = L"textBox8";
-			this->textBox8->Size = System::Drawing::Size(170, 26);
+			this->textBox8->Size = System::Drawing::Size(115, 20);
 			this->textBox8->TabIndex = 8;
 			// 
 			// textBox9
 			// 
-			this->textBox9->Location = System::Drawing::Point(23, 100);
+			this->textBox9->Location = System::Drawing::Point(15, 65);
+			this->textBox9->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->textBox9->Multiline = true;
 			this->textBox9->Name = L"textBox9";
-			this->textBox9->Size = System::Drawing::Size(170, 26);
+			this->textBox9->Size = System::Drawing::Size(115, 18);
 			this->textBox9->TabIndex = 9;
 			// 
 			// Stock
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1063, 575);
+			this->ClientSize = System::Drawing::Size(709, 374);
 			this->Controls->Add(this->textBox9);
 			this->Controls->Add(this->textBox8);
 			this->Controls->Add(this->textBox7);
@@ -161,6 +171,7 @@ namespace Interface1 {
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->dataGridView1);
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->Name = L"Stock";
 			this->Text = L"Stock";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
